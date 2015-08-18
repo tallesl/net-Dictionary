0b916f0eb7177b9f50ad1b340de6d0d65df109cc
Talles L <talleslasmar@gmail.com>
2015-08-18 11:07:20 -0300

    tests

---
+++ Tests/SomeData.cs
@@ -0,0 +1,15 @@
+﻿namespace PropertiesHash.Tests
+{
+    using System;
+
+    public class SomeData
+    {
+        public Guid Id { get; set; }
+
+        public int Integer { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime? Date { get; set; }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,79 @@
+﻿namespace PropertiesHash.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Dynamic;
+
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Typical()
+        {
+            // Arrange
+            var obj =
+                new SomeData()
+                {
+                    Id = new Guid("8d995e6e-af88-4043-8c31-8ba04c6b4298"),
+                    Integer = 23,
+                    Text = "foo bar",
+                    Date = null
+                };
+
+            // Act
+            var dict = PropertiesHasher.Make(obj);
+
+            // Assert
+            Assert.AreEqual(obj.Id, dict["Id"]);
+            Assert.AreEqual(obj.Integer, dict["Integer"]);
+            Assert.AreEqual(obj.Text, dict["Text"]);
+            Assert.AreEqual(obj.Date, dict["Date"]);
+        }
+
+        [TestMethod]
+        public void Expando()
+        {
+            // Arrange
+            dynamic obj = new ExpandoObject();
+            obj.Id = new Guid("8d995e6e-af88-4043-8c31-8ba04c6b4298");
+            obj.Integer = 23;
+            obj.Text = "foo bar";
+            obj.Date = (DateTime?)null;
+
+            // Act
+            // If I use "var dict" here the type of dict is an ExpandoObject...
+            // What on earth is that?
+            IDictionary<string, object> dict = PropertiesHasher.Make(obj);
+
+            // Assert
+            Assert.AreEqual(obj.Id, dict["Id"]);
+            Assert.AreEqual(obj.Integer, dict["Integer"]);
+            Assert.AreEqual(obj.Text, dict["Text"]);
+            Assert.AreEqual(obj.Date, dict["Date"]);
+        }
+
+        [TestMethod]
+        public void Anonymous()
+        {
+            // Arrange
+            var obj =
+                new
+                {
+                    Id = new Guid("8d995e6e-af88-4043-8c31-8ba04c6b4298"),
+                    Integer = 23,
+                    Text = "foo bar",
+                    Date = (DateTime?)null
+                };
+
+            // Act
+            var dict = PropertiesHasher.Make(obj);
+
+            // Assert
+            Assert.AreEqual(obj.Id, dict["Id"]);
+            Assert.AreEqual(obj.Integer, dict["Integer"]);
+            Assert.AreEqual(obj.Text, dict["Text"]);
+            Assert.AreEqual(obj.Date, dict["Date"]);
+        }
+    }
+}
